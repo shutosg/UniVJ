fd81028a9fbffa0da4ca304f4e5344a71d094730
shutosg <shutoreds@gmail.com>
2020-03-01 14:13:26 +0900

    [Extension] add: GameObjectExtension

---
+++ Assets/UniVJ/Utilities/Extensions/GameObjectExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class GameObjectExtension
+{
+    public static void SetLayerRecursively(this GameObject self, int layer)
+    {
+        self.layer = layer;
+        foreach(Transform t in self.transform)
+        {
+            SetLayerRecursively(t.gameObject, layer);
+        }
+    }
+}
