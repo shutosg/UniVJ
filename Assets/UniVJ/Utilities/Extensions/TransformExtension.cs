e165b0db213f1447628bf6de935a069ad12864f9
shutosg <shutoreds@gmail.com>
2020-03-16 09:00:00 +0900

    update: Simple Scene

--- Assets/UniVJ/Utilities/Extensions/TransformExtension.cs
+++ Assets/UniVJ/Utilities/Extensions/TransformExtension.cs
@@ -7,26 +7,37 @@ public static class TransformExtension
     /// </summary>
     /// <param name="self"></param>
     /// <param name="x"></param>
-    public static void AddPositionX(this Transform self, float x) => self.position = self.position.AddX(x);
-    public static void AddPositionY(this Transform self, float y) => self.position = self.position.AddY(y);
-    public static void AddPositionZ(this Transform self, float z) => self.position = self.position.AddZ(z);
+    public static void AddPositionX(this Transform self, float v) => self.position = self.position.AddX(v);
+    public static void AddPositionY(this Transform self, float v) => self.position = self.position.AddY(v);
+    public static void AddPositionZ(this Transform self, float v) => self.position = self.position.AddZ(v);
 
     /// <summary>
     /// 回転に x を加える
     /// </summary>
     /// <param name="self"></param>
     /// <param name="x"></param>
-    public static void AddRotationX(this Transform self, float x) => self.rotation = self.rotation.AddX(x);
-    public static void AddRotationY(this Transform self, float y) => self.rotation = self.rotation.AddY(y);
-    public static void AddRotationZ(this Transform self, float z) => self.rotation = self.rotation.AddZ(z);
-    public static void AddRotationW(this Transform self, float w) => self.rotation = self.rotation.AddW(w);
+    public static void AddRotationX(this Transform self, float v) => self.rotation = self.rotation.AddX(v);
+    public static void AddRotationY(this Transform self, float v) => self.rotation = self.rotation.AddY(v);
+    public static void AddRotationZ(this Transform self, float v) => self.rotation = self.rotation.AddZ(v);
+    public static void AddRotationW(this Transform self, float v) => self.rotation = self.rotation.AddW(v);
 
     /// <summary>
     /// 回転に x を加える
     /// </summary>
     /// <param name="self"></param>
     /// <param name="x"></param>
-    public static void AddEulerAngleX(this Transform self, float x) => self.eulerAngles = self.eulerAngles.AddX(x);
-    public static void AddEulerAngleY(this Transform self, float y) => self.eulerAngles = self.eulerAngles.AddY(y);
-    public static void AddEulerAngleZ(this Transform self, float z) => self.eulerAngles = self.eulerAngles.AddZ(z);
+    public static void AddEulerAngleX(this Transform self, float v) => self.eulerAngles = self.eulerAngles.AddX(v);
+    public static void AddEulerAngleY(this Transform self, float v) => self.eulerAngles = self.eulerAngles.AddY(v);
+    public static void AddEulerAngleZ(this Transform self, float v) => self.eulerAngles = self.eulerAngles.AddZ(v);
+    public static void AddEulerAngle(this Transform self, float v) => self.eulerAngles = self.eulerAngles.Add(v);
+
+    /// <summary>
+    /// スケールをセットする
+    /// </summary>
+    /// <param name="self"></param>
+    /// <param name="s"></param>
+    public static void SetLocalScaleX(this Transform self, float v) => self.localScale = self.localScale.SetX(v);
+    public static void SetLocalScaleY(this Transform self, float v) => self.localScale = self.localScale.SetY(v);
+    public static void SetLocalScaleZ(this Transform self, float v) => self.localScale = self.localScale.SetZ(v);
+    public static void SetLocalScale(this Transform self, float v) => self.localScale = self.localScale.Set(v);
 }
---
+++ Assets/UniVJ/Utilities/SimpleTransform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SimpleTransform : MonoBehaviour
+{
+    [SerializeField] private float _rotationPerFrame;
+    [SerializeField] private float _scaleAmplitude;
+
+    private Transform _cacheTrans;
+    private Transform _cachedTrans => _cacheTrans ? _cacheTrans : (_cacheTrans = transform);
+    private Vector3 _scaleCache;
+
+    private void Start()
+    {
+        _scaleCache = transform.localScale;
+    }
+
+    void Update()
+    {
+        _cachedTrans.AddEulerAngleY(_rotationPerFrame);
+        _cachedTrans.localScale = _scaleCache.Add(Mathf.Sin(Time.time) * _scaleAmplitude);
+    }
+}
