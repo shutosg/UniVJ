1f09865a44f5ac4dca598d6f86ccf743d9f6edcd
shutosg <shutoreds@gmail.com>
2020-03-16 09:17:16 +0900

    update: VFXRing input

---
+++ Assets/UniVJ/Scenes/SubScenes/VFXRingManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+using UnityEngine.Video;
+using UniRx.Async;
+using UnityEngine.VFX;
+
+public class VFXRingManager : SubSceneManager
+{
+    [SerializeField] private VisualEffect _vfxRing;
+
+    public override void OnReceiveAttack(float value)
+    {
+        _vfxRing.SetFloat("Radius", value * 10);
+    }
+
+    public override void OnReceiveSpeed(float value)
+    {        
+        _vfxRing.SetFloat("ParticleSize", value);
+    }
+}
