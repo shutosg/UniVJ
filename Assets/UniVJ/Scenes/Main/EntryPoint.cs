2c7276b2f3feb6870547d85af82a1ba8b4da25c0
shutosg <shutoreds@gmail.com>
2020-03-22 00:13:57 +0900

    add: MidiInputBinder

--- Assets/UniVJ/Scenes/Main/EntryPoint.cs
+++ Assets/UniVJ/Scenes/Main/EntryPoint.cs
@@ -8,6 +8,7 @@ using UniRx.Async;
 public class EntryPoint : MonoBehaviour
 {
     [SerializeField] private ControlPanel _controlPanel;
+    [SerializeField] private MidiInputBinder _binder;
 
     void Start()
     {
@@ -17,5 +18,16 @@ public class EntryPoint : MonoBehaviour
             d.Activate();
         }
         _controlPanel.Initialize();
+
+        // nano KONTROL2 で使う knobNumber
+        _binder.Initialize(new[]
+        {
+            0, 1, 2, 3, 4, 5, 6, 7,
+            16, 17, 18, 19, 20, 21, 22, 23,
+            32, 33, 34, 35, 36, 37, 38, 39,
+            41, 42, 43, 44, 45, 46,
+            48, 49, 50, 51, 52, 53, 54, 55, 58, 59, 60, 61, 62,
+            64, 65, 66, 67, 68, 69, 71
+        });
     }
 }
---
+++ Assets/UniVJ/Utilities/MidiInputBinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MidiJack;
+using UnityEngine;
+
+public class MidiInputBinder : MonoBehaviour
+{
+    private const float Tolerance = 0.0000001f;
+    private readonly List<int> _knobNumbers = new List<int>();
+    private readonly Dictionary<int, float> _previousValues = new Dictionary<int, float>();
+
+    private bool _isInitialized;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    /// <param name="knobNumbers">リッスンするMIDIコントローラーのknobNumber</param>
+    public void Initialize(int[] knobNumbers)
+    {
+        // 受け取った knobNumber 配列を元に _previousValues 初期化
+        _knobNumbers.AddRange(knobNumbers);
+        _knobNumbers.ForEach(knobNumber => _previousValues[knobNumber] = 0);
+        _isInitialized = true;
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        if (!_isInitialized) return;
+        checkValues(_knobNumbers);
+    }
+
+    /// <summary>
+    /// 対象の knobNumber の値に変化があればイベントを発火する
+    /// </summary>
+    /// <param name="targetNumbers">調べるknobNumberの配列</param>
+    private void checkValues(List<int> targetNumbers)
+    {
+        foreach (var knobNumber in targetNumbers)
+        {
+            var value = MidiMaster.GetKnob(knobNumber);
+            if (!(Math.Abs(_previousValues[knobNumber] - value) > Tolerance)) continue;
+            _previousValues[knobNumber] = value;
+            Debug.Log(knobNumber + ": " + value);
+        }
+    }
+}
