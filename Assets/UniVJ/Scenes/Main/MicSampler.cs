bd0886d0d4f44bad2a54c44fed278eb80290e936
shutosg <shutoreds@gmail.com>
2020-03-24 04:01:17 +0900

    [MicSampler] update: adjust for general use

--- Assets/UniVJ/Scenes/Main/MicSampler.cs
+++ Assets/UniVJ/Scenes/Main/MicSampler.cs
@@ -1,32 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx.Async;
 
 public class MicSampler : MonoBehaviour
 {
-    [SerializeField] AudioSource source;
+    [SerializeField] AudioSource _source;
     [SerializeField, Range(0f, 10f)] float _gain = 1f; // 音量に掛ける倍率
 
-    private float[] _volumeRates = new float[1024];
+    public bool IsInitialized { get; private set; }
+    private float[] _spectrum;
 
-    void Start()
+    public async UniTask Initialize(float[] spectrum)
     {
-        Debug.Log(Microphone.devices.Length);
-        if ((Microphone.devices.Length > 0))
+        if ((Microphone.devices.Length == 0))
         {
-            source.clip = Microphone.Start(null, true, 10, 44100);
-            while (!(Microphone.GetPosition(null) > 0)) { }
-            source.Play();
+            Debug.LogError("マイクが認識されていません");
+            return;
         }
+
+        _spectrum = spectrum;
+        _source.loop = true;
+        _source.clip = Microphone.Start(null, true, 10, 44100);
+        // マイクの準備が整うまで待つ
+        await UniTask.WaitUntil(() => Microphone.GetPosition(null) > 0);
+        _source.Play();
+        IsInitialized = true;
     }
 
-    void Update()
+    public void UpdateValues()
     {
-        source.GetOutputData(_volumeRates, 1);
-        for (var i = 0; i < _volumeRates.Length; i++)
+        if (!IsInitialized) return;
+        _source.GetSpectrumData(_spectrum, 0, FFTWindow.Hamming);
+        for (var i = 0; i < _spectrum.Length; i++)
         {
-            _volumeRates[i] *= _gain;
+            _spectrum[i] *= _gain;
         }
-        _volumeRates.DebugLog();
     }
 }
