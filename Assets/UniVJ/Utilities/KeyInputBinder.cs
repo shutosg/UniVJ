7d8ed4bca64c7386b5fd30af4bc0cac8686d6d84
shutosg <shutoreds@gmail.com>
2020-03-25 16:55:41 +0900

    [Performance] fix: avoid every frame gc alloc

--- Assets/UniVJ/Utilities/KeyInputBinder.cs
+++ Assets/UniVJ/Utilities/KeyInputBinder.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class KeyInputBinder : MonoBehaviour
 {
     [SerializeField] private EventDictionary _events;
+    private KeyValuePair<KeyCode, UnityEvent>[] _keyValuePairs;
+
+    void Start()
+    {
+        _keyValuePairs = _events.ToArray();
+    }
 
     void Update()
     {
-        foreach (var keyValuePair in _events)
+        foreach (var keyValuePair in _keyValuePairs)
         {
             if (Input.GetKey(keyValuePair.Key))
             {
