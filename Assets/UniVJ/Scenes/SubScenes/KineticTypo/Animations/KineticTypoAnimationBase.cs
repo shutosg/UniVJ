8825d6ecc9500c58a75de73895f421844e4fe081
shutosg <shutoreds@gmail.com>
2020-07-03 01:46:25 +0900

    [TextAnimation] refactor: make animation class
    move core animation process from KineticTypoManager to KineticTypoAnimationBase and FromRandomAnimation

---
+++ Assets/UniVJ/Scenes/SubScenes/KineticTypo/Animations/KineticTypoAnimationBase.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using UnityEngine;
+using UniRx.Async;
+using TMPro;
+using UnityEngine.Serialization;
+using UniVJ.Utility;
+using Zenject;
+
+
+namespace KineticTypo
+{
+    public abstract class KineticTypoAnimationBase : MonoBehaviour
+    {
+        protected const float MinSpeed = 0.001f;
+        protected const float DefaultDuration = 0.5f;
+        [Inject(Id = InstallerId.MainTextId)] protected TextMeshPro _text;
+        [SerializeField] protected AnimationCurve _positionCurve;
+        [SerializeField] protected AnimationCurve _colorCurve;
+        [SerializeField] public string AnimationName;
+        public float Speed { get; protected set; } = 1f;
+        public float IntervalText { get; protected set; } = 0.025f;
+        private List<AnimationData> _animationDataList = new List<AnimationData>();
+        private TMP_FontAsset _font;
+
+        public void Initialize(TMP_FontAsset font) => _font = font;
+
+        public async UniTask PlayAsync(CancellationToken token)
+        {
+            _text.ForceMeshUpdate();
+
+            var textInfo = _text.textInfo;
+            var charCount = textInfo.characterCount;
+            var currentTime = Time.time;
+            var startTime = Time.time;
+            // カーブにキーが無ければ適当に打つ
+            if (_positionCurve.keys.Length == 0)
+            {
+                _positionCurve.AddKey(new Keyframe(0, 0));
+                _positionCurve.AddKey(new Keyframe(1, 1));
+            }
+            var curveDuration = _positionCurve.keys[_positionCurve.length - 1].time;
+
+            // 初期設定
+            for (var i = 0; i < charCount; i++)
+            {
+                var (positionOffset, timeOffset) = getStartState(i);
+                var matIndex = textInfo.characterInfo[i].materialReferenceIndex;
+                var charInfo = textInfo.characterInfo[i];
+                var sourceVertices = textInfo.meshInfo[matIndex].vertices;
+                var sourceColors = textInfo.meshInfo[matIndex].colors32;
+                // 足りなかったら作る、足りるなら reset
+                if (_animationDataList.Count <= i)
+                {
+                    var animData = new AnimationData(positionOffset, timeOffset, curveDuration, sourceVertices, charInfo.vertexIndex, sourceColors);
+                    _animationDataList.Add(animData);
+                }
+                else
+                {
+                    _animationDataList[i].Reset(positionOffset, timeOffset, curveDuration, sourceVertices, charInfo.vertexIndex, sourceColors);
+                }
+            }
+            // 多かったら消す
+            if (_animationDataList.Count > charCount) _animationDataList.RemoveRange(charCount, _animationDataList.Count - charCount);
+
+
+            // アニメーション
+            while (true)
+            {
+                currentTime += Time.deltaTime;
+                for (var i = 0; i < charCount; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    var animData = _animationDataList[i];
+                    // 終了してたら抜ける
+                    if (animData.IsCompleted) continue;
+                    // 時刻更新
+                    animData.CurrentTime = ((currentTime - startTime) - animData.TimeOffset) / (DefaultDuration / Speed);
+                    var isStarted = animData.CurrentTime > 0f;
+                    textInfo.characterInfo[i].isVisible = isStarted;
+                    var charInfo = textInfo.characterInfo[i];
+                    if (!charInfo.isVisible) continue;
+
+                    // アニメーション開始してるなら計算
+                    if (!isStarted) continue;
+                    calculateNewState(animData);
+                    var vertices = textInfo.meshInfo[charInfo.materialReferenceIndex].vertices;
+                    var vertIdx = charInfo.vertexIndex;
+                    var v = animData.CurrentVertices;
+                    vertices[vertIdx + 0] = v[0];
+                    vertices[vertIdx + 1] = v[1];
+                    vertices[vertIdx + 2] = v[2];
+                    vertices[vertIdx + 3] = v[3];
+
+                    var colors = textInfo.meshInfo[charInfo.materialReferenceIndex].colors32;
+                    var c = animData.CurrentColors;
+                    colors[vertIdx + 0] = c[0];
+                    colors[vertIdx + 1] = c[1];
+                    colors[vertIdx + 2] = c[2];
+                    colors[vertIdx + 3] = c[3];
+                }
+                token.ThrowIfCancellationRequested();
+                // 更新
+                for (var i = 0; i < textInfo.meshInfo.Length; i++)
+                {
+                    textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                    _text.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+                }
+                _text.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+                if (_animationDataList.All(d => d.IsCompleted)) return;
+
+                await UniTask.Yield();
+            }
+        }
+
+        /// <summary>
+        /// 各文字の初期座標の取得
+        /// </summary>
+        /// <param name="index">文字index</param>
+        /// <returns>初期状態の座標オフセット、アニメーションの開始オフセット</returns>
+        protected abstract (Vector3 positionOffset, float timeOffset) getStartState(int index);
+
+        /// <summary>
+        /// 各文字の座標を計算
+        /// </summary>
+        /// <param name="animData">対象の文字のAnimationData</param>
+        protected abstract void calculateNewState(AnimationData animData);
+
+        public void SetSpeed(float value) => Speed = Mathf.Max(value, MinSpeed);
+
+        public void SetIntervalText(float value) => IntervalText = value;
+
+        public void SetDuration(float value) => SetSpeed(DefaultDuration / value);
+    }
+}
--- Assets/UniVJ/Scenes/SubScenes/KineticTypo/KineticTypoManager.cs
+++ Assets/UniVJ/Scenes/SubScenes/KineticTypo/KineticTypoManager.cs
@@ -7,163 +7,95 @@ using TMPro;
 using UniRx;
 using UniRx.Async;
 using UniVJ;
-using UniVJ.Utility;
-using Random = UnityEngine.Random;
 using Zenject;
 
-public class KineticTypoManager : SubSceneManager
+namespace KineticTypo
 {
-    private const float MinSpeed = 0.001f;
-    private const float DefaultDuration = 0.5f;
-    private readonly Vector3 FarAway = Vector3.up * 100000f;
-    [Inject] private FootageManager _footageManager;
-    [SerializeField] private TextMeshPro _text;
-    [SerializeField] private AnimationCurve _curve;
-    private CancellationTokenSource _source;
-    private float _speed = 1f;
-    private float _intervalText = 0.025f;
-    private bool _isLoop;
-    private float _intervalAnimation = 0.5f;
-    private List<TMP_FontAsset> _fontAssets;
-    private List<Material> _materials;
-
-    public IObservable<Unit> OnAnimationCompleted => _onAnimationCompleted;
-    private readonly Subject<Unit> _onAnimationCompleted = new Subject<Unit>();
-
-    /// <summary>
-    /// 初期化
-    /// </summary>
-    /// <returns></returns>
-    public (List<TMP_FontAsset> Fonts, List<Material> Materials) Initialize()
+    public class KineticTypoManager : SubSceneManager
     {
-        _fontAssets = _footageManager.GetFonts().ToList();
-        _materials = _footageManager.GetFontMaterials().ToList();
-        return (_fontAssets, _materials);
-    }
+        [Inject] private FootageManager _footageManager;
+        [Inject(Id = InstallerId.MainTextId)] private TextMeshPro _text;
+        private CancellationTokenSource _source;
+        private bool _isLoop;
+        private float _intervalAnimation = 0.5f;
+        private List<TMP_FontAsset> _fontAssets;
+        private List<Material> _materials;
+        private List<Type> _animationTypes;
+        private KineticTypoAnimationBase _currentAnimation;
+        private int _nextAnimationIndex;
+        private KineticTypoAnimationBase[] _animations;
 
-    public override void OnReceiveSpeed(float value) => setAnimationSpeed(value);
-    protected override void onReceiveVariable1(float value) => setAnimationSpeed(DefaultDuration / value);
-    protected override void onReceiveVariable2(float value) => _intervalText = value;
-    protected override void onReceiveVariable3(float value) => _intervalAnimation = value;
+        public IObservable<Unit> OnAnimationCompleted => _onAnimationCompleted;
+        private readonly Subject<Unit> _onAnimationCompleted = new Subject<Unit>();
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="value"></param>
-    protected void setAnimationSpeed(float value) => _speed = Mathf.Max(value, MinSpeed);
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="text"></param>
-    public void PlayAnimation(string text)
-    {
-        _text.text = text;
-        loopPlay().Forget();
-
-        async UniTask loopPlay() {
-            do {
-                await Replay();
-                await UniTask.Delay((int)(_intervalAnimation * 1000));
-            } while (_isLoop);
-        }
-    }
-
-    public async UniTask Replay()
-    {
-        _source?.Cancel();
-        _source = new CancellationTokenSource();
-        await play(_source.Token);
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="token"></param>
-    /// <returns></returns>
-    private async UniTask play(CancellationToken token)
-    {
-        const int VertexCountPerChar = 4;
-        // ジオメトリ更新
-        _text.ForceMeshUpdate();
-
-        var textInfo = _text.textInfo;
-        var charCount = textInfo.characterCount;
-        var currentTime = Time.time;
-        var startTime = Time.time;
-        // 文字ごとのアニメーションが最後まで再生されたか
-        var completes = new bool[charCount];
-        var posOffsets = new Vector3[charCount];
-        var startOffsets = new float[charCount];
-        var sourceVertices = new Vector3[charCount][];
-        var curveDuration = _curve.keys[_curve.length - 1].time;
-
-        // 初期設定
-        for (var i = 0; i < charCount; i++)
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        /// <returns></returns>
+        public (List<TMP_FontAsset> Fonts, List<Material> Materials, KineticTypoAnimationBase[] Animations) Initialize()
         {
-            startOffsets[i] = i * _intervalText / _speed;
-            var startX = 0f;
-            while (Mathf.Abs(startX) < 5f) startX = Random.Range(-10f, 10f);
-            var startY = 0f;
-            while (Mathf.Abs(startY) < 2f) startY = Random.Range(-10f, 10f);
-            posOffsets[i] = new Vector3(startX, startY, 0);
-            var matIndex = textInfo.characterInfo[i].materialReferenceIndex;
-            var charInfo = textInfo.characterInfo[i];
-            sourceVertices[i] = new Vector3[VertexCountPerChar];
-            Array.Copy(textInfo.meshInfo[matIndex].vertices, charInfo.vertexIndex, sourceVertices[i], 0, VertexCountPerChar);
+            _fontAssets = _footageManager.GetFonts().ToList();
+            _materials = _footageManager.GetFontMaterials().ToList();
+            _animations = GetComponents<KineticTypoAnimationBase>();
+            return (_fontAssets, _materials, _animations);
         }
 
-        // アニメーション
-        while (true)
+        public override void OnReceiveSpeed(float value) => _currentAnimation?.SetSpeed(value);
+        protected override void onReceiveVariable1(float value) => _currentAnimation?.SetDuration(value);
+        protected override void onReceiveVariable2(float value) => _currentAnimation?.SetIntervalText(value);
+        protected override void onReceiveVariable3(float value) => _intervalAnimation = value;
+
+        /// <summary>
+        /// 再生
+        /// </summary>
+        /// <param name="text"></param>
+        public void PlayAnimation(string text)
         {
-            token.ThrowIfCancellationRequested();
-            currentTime += Time.deltaTime;
-            for (var i = 0; i < charCount; i++)
+            var nextAnimation = _animations[_nextAnimationIndex];
+            // アニメーションパラメータを次のアニメーションに引き継ぐ
+            if (_currentAnimation != null)
             {
-                // 非表示なら無視
-                var charInfo = textInfo.characterInfo[i];
-                var animationTime = ((currentTime - startTime) - startOffsets[i]) / (DefaultDuration / _speed);
-                var isStarted = animationTime > 0f;
-                completes[i] = MathUtility.NearlyEquals(animationTime, curveDuration) || animationTime > curveDuration;
-                if (!charInfo.isVisible) continue;
-
-                var offset = isStarted ? MathUtility.Lerp(posOffsets[i], Vector3.zero, _curve.Evaluate(animationTime)) : FarAway;
-                var vertices = textInfo.meshInfo[charInfo.materialReferenceIndex].vertices;
-                var vertIdx = charInfo.vertexIndex;
-                var v = sourceVertices[i][0];
-                vertices[vertIdx + 0] = sourceVertices[i][0] + offset;
-                vertices[vertIdx + 1] = sourceVertices[i][1] + offset;
-                vertices[vertIdx + 2] = sourceVertices[i][2] + offset;
-                vertices[vertIdx + 3] = sourceVertices[i][3] + offset;
-            }
-            // 更新
-            for (var i = 0; i < textInfo.meshInfo.Length; i++)
-            {
-                textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
-                _text.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+                nextAnimation.SetSpeed(_currentAnimation.Speed);
+                nextAnimation.SetIntervalText(_currentAnimation.IntervalText);
             }
 
-            if (completes.All(completed => completed))
+            _currentAnimation = nextAnimation;
+
+            _text.text = text;
+            loopPlay().Forget();
+
+            async UniTask loopPlay()
             {
-                _onAnimationCompleted.OnNext(Unit.Default);
-                return;
+                do
+                {
+                    await Replay();
+                    await UniTask.Delay((int)(_intervalAnimation * 1000), cancellationToken: _source.Token);
+                } while (_isLoop);
             }
-            await UniTask.Yield();
         }
+
+        public async UniTask Replay()
+        {
+            _source?.Cancel();
+            _source = new CancellationTokenSource();
+            await _currentAnimation.PlayAsync(_source.Token);
+            _onAnimationCompleted.OnNext(Unit.Default);
+        }
+
+        public void SetFont(TMP_FontAsset font)
+        {
+            _text.font = font;
+            // マテリアルが見つかれば割り当てる
+            var targetMaterial = _materials.FirstOrDefault(m => m.name.IndexOf(font.name + "_OutlineOnly") != -1);
+            if (targetMaterial != null) _text.fontSharedMaterial = targetMaterial;
+        }
+
+        public void SetAnimation(int index)
+        {
+            _nextAnimationIndex = index;
+            PlayAnimation(_text.text);
+        }
+
+        public void SetLoop(bool isLoop) => _isLoop = isLoop;
     }
-
-    public void SetFont(TMP_FontAsset font)
-    {
-        _text.font = font;
-        // マテリアルが見つかれば割り当てる
-        var targetMaterial = _materials.FirstOrDefault(m => m.name.IndexOf(font.name + "_OutlineOnly") != -1);
-        if (targetMaterial != null) _text.fontSharedMaterial = targetMaterial;
-    }
-
-    public void SetLoop(bool isLoop) => _isLoop = isLoop;
-}
-
-public enum KineticType
-{
-    Simple,
 }
